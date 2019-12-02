1e829701c2b427e96d6d520ca4288ba87399257e
Anandu B Ajith <reach2anandu@gmail.com>
2019-12-02 20:41:38 +0530

    enhancements

--- ragam-admin/src/App.vue
+++ ragam-admin/src/App.vue
@@ -1,17 +1,14 @@
 <template>
 <div id="app">
-  <nav class="navbar" role="navigation" aria-label="main navigation">
+  <nav class="navbar is-primary" role="navigation" aria-label="main navigation">
     <div class="navbar-brand">
       <router-link class="navbar-item" to="/home" >
         <h4 class="is-size-3">Ragam CA Admin</h4>
       </router-link>
     </div>
   </nav>
-
-
   <div class="container">
     <router-view/>
   </div>
-
 </div>
 </template>
