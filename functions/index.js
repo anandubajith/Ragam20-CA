0d8ad6cd1033a8e869672899eb7be99e989c4036
Anandu B Ajith <reach2anandu@gmail.com>
2019-12-02 18:57:21 +0530

    add cloud-function to generate leaderboard

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,35 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+admin.initializeApp();
+var db = admin.database();
+
+// Rewrite this to new form, 
+exports.leaderboard = functions.database.ref('/points').onWrite(() => {
+    return db.ref('ambassadors').on('value', function(snap) {
+        let ambassadors = snap.val();
+        return db.ref('points').on('value', function(snapshot) {
+            let data = (snapshot.val());
+            let result = [];
+            for (let key of Object.keys(data)) {
+                if (key != '0g4wwx5HpuY1MHcdrD12Xn6Kik43') {
+                    if (ambassadors[key] && ambassadors[key].name) {
+                        result.push({
+                            name: ambassadors[key].name,
+                            point: parseInt(data[key][0], 10),
+                            id: key
+                        });
+                    }
+                }
+            }
+
+            let sorted = result.sort(function(a, b) {
+                if (a.point > b.point) return -1;
+                if (a.point < b.point) return 1;
+                return 0;
+            });
+
+            return db.ref('leaderboard').set(sorted);
+        });
+    });
+});
