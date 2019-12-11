3ad5206acbb1466b29ab6e44f9698567435372e7
Anandu B Ajith <reach2anandu@gmail.com>
2019-12-11 23:00:55 +0530

    complete addReferral function

--- helpers/parse_referral.js
+++ helpers/parse_referral.js
@@ -1,12 +1,36 @@
 // Get name and referral code from XLS file
 // call add_referral
 
+const DRY_RUN = true;
+
+
+var admin = require("firebase-admin");
 const ref_obj = require('./ref_codes.json');
+const serviceAccount = require("./service-key.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://ragam-ca.firebaseio.com"
+});
+
+const db = admin.database();
+
+
 function add_referral(ref_code, name, workshopName) {
-
-    let uid = ref_obj[ref_code];
-
+    // check if ref_code is valid
+    if ( !(ref_code in ref_obj) ) {
+        console.error(`Invalid referral code: ${ref_code}`);
+        console.error(`${name} - ${workshopName}`);
+        return;
+    }
+    const uid = ref_obj[ref_code]; 
     const { key } = db.ref(`ambassadors/${uid}/referral`).push();
+    
+    console.info(`${ref_code}: Adding referral ${name} - ${workshopName}`);
+
+    // just for safety, don't commit any transactions
+    if ( DRY_RUN )
+        return;
 
     db.ref(`ambassadors/${uid}/referral/${key}`)
       .update({
@@ -21,8 +45,4 @@ function add_referral(ref_code, name, workshopName) {
             value: 2000,
           }),
       );
-
-    console.log(`${ref_code}: Added referral ${name} - ${workshopName}`);
 }
-
-
